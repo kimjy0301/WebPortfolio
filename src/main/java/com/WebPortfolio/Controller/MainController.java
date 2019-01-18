5e04a1a640717fb3dd61539a35f2e152a90b070f
KimJiYong <KimJiYong@125.142.67.246>
2019-01-18 20:26:26 +0900

    Service분리

--- src/main/java/com/WebPortfolio/Controller/MainController.java
+++ src/main/java/com/WebPortfolio/Controller/MainController.java
@@ -1,123 +1,45 @@
 package com.WebPortfolio.Controller;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import com.WebPortfolio.Model.Instagram;
-import com.WebPortfolio.Model.Message;
-import com.WebPortfolio.Model.Portfolio;
-import com.WebPortfolio.Reporitory.MessageRepository;
-import com.WebPortfolio.Reporitory.PortfolioImgReporitory;
-import com.WebPortfolio.Reporitory.PortfolioReporitory;
-
-import lombok.extern.slf4j.Slf4j;
+import com.WebPortfolio.Service.InstagramService;
+import com.WebPortfolio.Service.MessageService;
+import com.WebPortfolio.Service.PortfolioService;
 
 @Controller
-@Slf4j
 public class MainController {
 
 	@Autowired
-	MessageRepository messageRepository;
-
+	InstagramService instagramService;
+	
 	@Autowired
-	PortfolioReporitory portfolioReporitory;
+	PortfolioService portfolioService;
+	
 	@Autowired
-	PortfolioImgReporitory portfolioImgReporitory;
-
-	@Value("${upload.uploadingdir}")
-	String uploadingdir;
-
+	MessageService messageService;	
+	
 	@Value("${upload.portfolioPath}")
 	String portfolioPath;
 
-	@Value("${aws.accessKey}")
-	String accessKey;
-
-	@Value("${aws.secretKey}")
-	String secretKey;
-
-	@Value("${instagram.accessToken}")
-	String instagramToken;
 
 	@RequestMapping("/")
-	@Transactional
 	public String home(Model model) throws JSONException {
-
-		log.debug("home Test");
-
-		Message ms = new Message();
-		model.addAttribute("formModel", ms);
-
-		Iterable<Message> list = messageRepository.findTop5ByOrderByIdDesc();
-		model.addAttribute("datalist", list);
-
-		List<Portfolio> list2 = portfolioReporitory.findAll();
-
-		/*
-		 * AwsS3 s3 = new AwsS3(accessKey,secretKey);
-		 * s3.createBucket("jiyong-portfolio");
-		 * 
-		 * for (Portfolio portfolio : list2) {
-		 * s3.getObject("jiyong-portfolio",portfolio.getId() + "/" +
-		 * portfolio.getThumbnailPath(), portfolioPath + portfolio.getThumbnailPath());
-		 * for (PortfolioImg portfolioimg : portfolio.getListPortfolioImg()) {
-		 * s3.getObject("jiyong-portfolio", portfolio.getId() + "/" +
-		 * portfolioimg.getFileName(), portfolioPath + portfolioimg.getFileName()); } }
-		 */
-
-		model.addAttribute("portfolioList", list2);
-		model.addAttribute("portfolioPath", portfolioPath);
-
-		RestTemplate restTemplate = new RestTemplate();
-
-		URI uri = UriComponentsBuilder.fromHttpUrl("https://api.instagram.com/v1/users/self/media/recent/")
-				.queryParam("access_token", instagramToken).queryParam("count", 12).build().toUri();
-
-		String response = restTemplate.getForObject(uri, String.class);
-		JSONObject json = new JSONObject(response);
-		JSONArray dataArray = json.getJSONArray("data");
 		
-		List<Instagram> instagramList = new ArrayList<Instagram>();		
-		
-		for(int i=0;i < dataArray.length();i++){                                     
-		    JSONObject obj = dataArray.getJSONObject(i);
-		    Instagram tmpInstagram = new Instagram();
-		    tmpInstagram.setLink(obj.getString("link"));
-		
-		    tmpInstagram.setImgPath(obj.getJSONObject("images").getJSONObject("standard_resolution").getString("url"));
-		    tmpInstagram.setLike(obj.getJSONObject("likes").getInt("count"));
-		    tmpInstagram.setComment(obj.getJSONObject("comments").getInt("count"));
-		    instagramList.add(tmpInstagram);
-		    
-		    
-		}
-					
-		
-		model.addAttribute("instagramList",instagramList);
+		model.addAttribute("datalist", messageService.getMessageListTop5ByOrderByIdDesc());
+		model.addAttribute("portfolioList", portfolioService.getPortfolioList());
+		model.addAttribute("portfolioPath", portfolioPath);		
+		model.addAttribute("instagramList",instagramService.getIntagramList());
 
 		return "index";
 	}
 
 	@RequestMapping("/index")
 	public String index2(Model model) {
-		log.debug("index page from redirect");
-
-		Iterable<Message> list = messageRepository.findTop5ByOrderByIdDesc();
-		model.addAttribute("datalist", list);
-
 		return "index";
 	}
 }
--- src/main/java/com/WebPortfolio/Controller/PortFolioController.java
+++ src/main/java/com/WebPortfolio/Controller/PortFolioController.java
@@ -1,27 +1,17 @@
 package com.WebPortfolio.Controller;
 
-import java.io.File;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.WebPortfolio.AWS.AwsS3;
-import com.WebPortfolio.Bean.ImgControl;
 import com.WebPortfolio.Model.Portfolio;
-import com.WebPortfolio.Model.PortfolioImg;
-import com.WebPortfolio.Reporitory.PortfolioImgReporitory;
-import com.WebPortfolio.Reporitory.PortfolioReporitory;
+import com.WebPortfolio.Service.PortfolioService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -29,106 +19,25 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequestMapping("/portfolio")
 public class PortFolioController {
-
-	@Autowired
-	ImgControl imgControl;
-
-	@Autowired
-	PortfolioReporitory portfolioReporitory;
-
-	@Autowired
-	PortfolioImgReporitory portfolioImgReporitory;
-
-		
-	@Value("${upload.uploadingdir}") 
-	String uploadingdir; 
-
-	@Value("${upload.portfolioPath}") 
-	String portfolioPath; 
 	
-	@Value("${aws.accessKey}") 
-	String accessKey;
-
-	@Value("${aws.secretKey}") 
-	String secretKey; 	
+	@Autowired
+	PortfolioService portfolioService;
 	
 
 	
 	@RequestMapping("/")
-	public String uploading(Model model) {
-
-		List<Portfolio> list = portfolioReporitory.findAll();		
-		model.addAttribute("list",list);
-		
-		
-		File file = new File(uploadingdir);
-		model.addAttribute("files", file.listFiles());
-		return "upload";
+	public String uploading(Model model) {	
+		model.addAttribute("list",portfolioService.getPortfolioList());		
+		return "upload";		
 	}
 
-	@Transactional
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public String uploadingPost(Portfolio portfolio, @RequestParam("input-b3[]") MultipartFile[] uploadingFiles,
 			HttpServletRequest req, Model model) throws Exception {
 
 
-		portfolioReporitory.save(portfolio);
+		portfolioService.uploadPortfolio(portfolio,uploadingFiles);
 		
-		for ( int i =0 ; i<uploadingFiles.length; i++) {
-			
-			
-	/*		AwsS3 s3 = new AwsS3(accessKey,secretKey);			
-			s3.createBucket("jiyong-portfolio");	
-			*/
-			
-	        MultipartFile uploadedFile = uploadingFiles[i];	 
-	        
-			File file = new File(uploadingdir + uploadedFile.getOriginalFilename());
-			File uploadPath = new File(uploadingdir);
-
-			if (uploadPath.exists() == false) {
-				uploadPath.mkdir();
-			}
-
-			if (!uploadedFile.isEmpty()) {
-				uploadedFile.transferTo(file);
-			
-				PortfolioImg pfImg = new PortfolioImg();				
-				portfolioImgReporitory.save(pfImg);
-				
-				String uploadedPath=imgControl.makeResizeImg(file, portfolio.getId() + "_" + i + ".JPG", portfolioPath, "JPG");
-				
-				if (i==0) {
-					String thumbPath=imgControl.makeThumbnail(file, portfolio.getId() + ".JPG", portfolioPath, "JPG");							
-					portfolio.setThumbnailPath(thumbPath);					
-					
-	/*				if (s3.putObejct("jiyong-portfolio", portfolio.getId() + "/" + thumbPath, portfolioPath + thumbPath)) {
-						File tmpFile = new File(portfolioPath + thumbPath);
-						if (tmpFile.exists()) {
-							tmpFile.delete();
-						}
-					}*/
-					
-				}
-				
-				pfImg.setFileName(uploadedPath);
-				pfImg.setPortfolio(portfolio);
-				
-				/*				
-				if (s3.putObejct("jiyong-portfolio", portfolio.getId() + "/" + uploadedPath, portfolioPath + uploadedPath)) {
-					File tmpFile = new File(portfolioPath + uploadedPath);
-					if (tmpFile.exists()) {
-						tmpFile.delete();
-					}
-				}
-				*/				
-				
-				if (file.exists()) {
-					file.delete();
-				}
-				
-			}
-		}
 				
 		return "redirect:/portfolio/";
 	}
